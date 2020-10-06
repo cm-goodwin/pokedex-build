0ad7d845f26c29636e03db4ed5c1fb06c10720ea
Chris Goodwin <69440123+cm-goodwin@users.noreply.github.com>
2020-10-06 00:59:31 -0400

    Added 7 pokemon
    added names, heights, and types

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-var pokemonList = [{name:}, {name:}, {name:}, {name:}, {name:}, {name:}, {name:}];
+var pokemonList = [
+  { name: "Torchic", height: 1, type: "Fire" },
+  { name: "Combusken", height: 2, type: ["Fire", "Fighting"] },
+  { name: "Marshtomp", height: 2, type: ["Water", "Ground"] },
+  { name: "Swampert", height: 4, type: ["Water", "Ground"] },
+  { name: "Grovyle", height: 2, type: "Grass" },
+  { name: "Sceptile", height: 5, type: "Grass" },
+  { name: "Rayquaza", height: 23, type: ["Dragon", "Flying"] },
+];

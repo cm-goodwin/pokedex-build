cf0e6c63b2c0245eeffcb2efc4a0d416b80eff42
Chris Goodwin <69440123+cm-goodwin@users.noreply.github.com>
2020-10-27 00:37:21 -0400

    forEachLoop, Local data
    Added a forEach Loop and added data to a local function so it wouldnt effect global values. Also added a getAll ft + an add ft~~ still tryin to figure that one out

--- js/script.js
+++ js/script.js
@@ -1,73 +1,88 @@
-const pokemonList = [
-  {
-    name: "Combusken",
-    height: 2.9,
-    type: ["Fire", "Fighting"],
-  },
-  {
-    name: "Blaziken",
-    height: 6,
-    type: ["Fire", "Fighting"],
-  },
-  {
-    name: "Marshtomp",
-    height: 2,
-    type: ["Water", "Ground"],
-  },
-  {
-    name: "Swampert",
-    height: 4.9,
-    type: ["Water", "Ground"],
-  },
-  { name: "Grovyle", height: 2.9, type: "Grass" },
-  { name: "Sceptile", height: 5.7, type: "Grass" },
-  {
-    name: "Rayquaza",
-    height: 23,
-    type: ["Dragon", "Flying"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Combusken",
+      height: 2.9,
+      type: ["Fire", "Fighting"],
+    },
+    {
+      name: "Blaziken",
+      height: 6,
+      type: ["Fire", "Fighting"],
+    },
+    {
+      name: "Marshtomp",
+      height: 2,
+      type: ["Water", "Ground"],
+    },
+    {
+      name: "Swampert",
+      height: 4.9,
+      type: ["Water", "Ground"],
+    },
+    { name: "Grovyle", height: 2.9, type: "Grass" },
+    { name: "Sceptile", height: 5.7, type: "Grass" },
+    {
+      name: "Rayquaza",
+      height: 23,
+      type: ["Dragon", "Flying"],
+    },
+  ];
 
-// pokemonList.push({
-//   name: "Combusken",
-//   height: 2.9,
-//   type: ["Fire", "Fighting"],
-// });
-// pokemonList.push({
-//   name: "Blaziken",
-//   height: 6,
-//   type: ["Fire", "Fighting"],
-// });
-// pokemonList.push({
-//   name: "Marshtomp",
-//   height: 2,
-//   type: ["Water", "Ground"],
-// });
-// pokemonList.push({
-//   name: "Swampert",
-//   height: 4.9,
-//   type: ["Water", "Ground"],
-// });
-// pokemonList.push({ name: "Grovyle", height: 2.9, type: "Grass" });
-// pokemonList.push({ name: "Sceptile", height: 5.7, type: "Grass" });
-// pokemonList.push({
-//   name: "Rayquaza",
-//   height: 23,
-//   type: ["Dragon", "Flying"],
-// });
-
-console.log("pokemonList: ", pokemonList);
-
-// for loop to iterate over pokemon list
-for (let i = 0; i < pokemonList.length; i++) {
-  // conditional logs pokemon with a height over 20
-  if (pokemonList[i].height > 20) {
-    document.write(
-      `<p class="pokemon">${pokemonList[i].name} (Height: ${pokemonList[i].height}) - Wow, like OMG, that's so big!</p>`
+  pokemonList.forEach(function (pokemon) {
+    console.log(
+      "Name: " +
+        pokemon.name +
+        " , Height: " +
+        pokemon.height +
+        " , Type: " +
+        pokemon.type
     );
-  } else {
     document.write(
-      `<p class="pokemon">${pokemonList[i].name} (Height: ${pokemonList[i].height})</p>`
+      "<p class='pokemon'>" +
+        "Name: " +
+        pokemon.name +
+        " , Height: " +
+        pokemon.height +
+        " , Type: " +
+        pokemon.type +
+        "</p>"
     );
+  });
+  //Cant seem to get this to work the right way Im goin kind of crazy :/ right now :)
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-}
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+//Wasnt sure what it meant by having the forEachLoop inside and outside of the iife cause if i un comment the section below it says the list isnt defined since it has been put locally in the iife
+
+/*pokemonList.forEach(function (pokemon) {
+  console.log(
+    "Name: " +
+      pokemon.name +
+      " , Height: " +
+      pokemon.height +
+      " , Type: " +
+      pokemon.type
+  );
+  document.write(
+    "<p class='pokemon'>" +
+      "Name: " +
+      pokemon.name +
+      " , Height: " +
+      pokemon.height +
+      " , Type: " +
+      pokemon.type +
+      "</p>"
+  );
+});*/
